37e002a67dd6afa5c1ba7856a12c177c39e42428
Prady KAENCHANH <prakaenchanh@gmail.com>
2025-04-08 10:30:38 +0200

    riviere pas fini !!!

--- modules/gestion_tuiles.py
+++ modules/gestion_tuiles.py
@@ -1,3 +1,32 @@
+
+
+
+
+def rivieres(grille,i,j, nom_tuile, h = {}):
+    for k in range(len(nom_tuile)):
+        if nom_tuile[k] == "R":
+            break
+        elif k == 3:
+            return True
+    
+    direction = [(-1, 0, 0), (0, 1, 1), (1, 0, 2), (0, -1, 3)]
+    t_true = [False]*4
+    for dx, dy, l in direction:
+        x, y = i + dx, j + dy
+        if 0 <= x < len(grille) and 0 <= y < len(grille[0]):
+            if (x,y) in list(h.keys()):
+                if l == (h[(x,y)] + 2) % 4:
+                    continue
+                else:
+                    return False
+            if nom_tuile[l] == "R":
+                    if grille[x][y] is not None:
+                        h[(x,y)] = l
+                        t_true[l] == rivieres(grille,x,y, grille[x][y], h = h)
+
+                
+        
+
 def emplacement_valide(grille: list[list[str]], i: int, j: int, nom_tuile: str):
     direction = [(-1, 0, 0), (0, 1, 1), (1, 0, 2), (0, -1, 3)] #(x, y, indice de la lettre a comparer)
     error = 0
